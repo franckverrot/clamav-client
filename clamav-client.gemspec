2d830fa1f0516acb6a3ec4360856230c7b2c53fa
Franck Verrot <franck@verrot.fr>
2014-02-21 23:02:35 +0100

    * clamav-client.gemspec : Add the minitest gem for older Rubies

--- clamav-client.gemspec
+++ clamav-client.gemspec
@@ -19,4 +19,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "pry"
+  spec.add_development_dependency "minitest"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'minitest/autorun'
+gem 'minitest'
 
+require 'minitest/autorun'
 require 'clamav/client'
