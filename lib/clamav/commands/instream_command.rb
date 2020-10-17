708bdd7337a364126c760b0bbde10c93b64955f0
Dieu Nguyen <dieu.nguyen@employmenthero.com>
2020-10-17 17:33:15 +0700

    Refactor to use an ENV of max_chunk_size of instream command

--- lib/clamav/commands/instream_command.rb
+++ lib/clamav/commands/instream_command.rb
@@ -19,10 +19,11 @@ require 'clamav/commands/command'
 module ClamAV
   module Commands
     class InstreamCommand < Command
+      DEFAULT_MAX_CHUNK_SIZE = 1024
 
-      def initialize(io, max_chunk_size = 1024)
+      def initialize(io)
         @io = begin io rescue raise ArgumentError, 'io is required', caller; end
-        @max_chunk_size = max_chunk_size
+        @max_chunk_size = ENV['CLAMAV_INSTREAM_MAX_CHUNK_SIZE'] || DEFAULT_MAX_CHUNK_SIZE
       end
 
       def call(conn)
